bd9d52fc2ceb57140d1c257e0d21beae5db85a92
Ninh <duyninh163@gmail.com>
2025-10-20 21:54:39 +0700

    add Footer component

--- frontend/src/sites/Login.jsx
+++ frontend/src/sites/Login.jsx
@@ -1,10 +1,14 @@
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function Login() {
     
 
     return (
-        <Navbar />
+        <>
+            <Navbar />
+            <Footer />
+        </>
     );
 }
 
