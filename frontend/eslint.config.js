e3a10c3101a01ac40a34ee2ea10441ae00eae078
Vongphet Pasithiphone <pasithphone.v220116@sis.hust.edu.vn>
2025-10-22 13:30:36 +0700

    fetch to ninh frontend

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,17 +2,17 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-
-export default [
-  { ignores: ['dist'] },
-  // Spread the recommended configs directly into the array
-  { ...js.configs.recommended },
-  { ...reactHooks.configs['recommended-latest'] },
-  { ...reactRefresh.configs.vite },
-  // Add custom config, overriding or extending as needed
+export default defineConfig([
+  globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
