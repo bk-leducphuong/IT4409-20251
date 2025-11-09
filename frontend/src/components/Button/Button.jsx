2c03f349ba31ca0861cf3bd0f20977a81349ee5e
Ninh <duyninh163@gmail.com>
2025-11-09 21:32:11 +0700

    chores: eslint and prettier fix

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,16 +1,24 @@
-import styles from "./Button.module.css";
+import styles from './Button.module.css';
 
 function Button(props) {
+  const style =
+    !props.backgroundColor || props.backgroundColor.toLowerCase() === 'red'
+      ? styles.red
+      : styles.white;
 
-    const style = 
-    (!props.backgroundColor || props.backgroundColor.toLowerCase() === "red") ?
-    styles.red : styles.white;
-
-    return (
-        <button className={style} onClick={props.onClick || (() => {console.log("button clicked")})}>
-            {props.children}
-        </button>
-    );
+  return (
+    <button
+      className={style}
+      onClick={
+        props.onClick ||
+        (() => {
+          console.log('button clicked');
+        })
+      }
+    >
+      {props.children}
+    </button>
+  );
 }
 
 export default Button;
