f9111a3ad86c1bacca4bf8b8a20cb59b750390f5
Le Duc Phuong <bkleducphuong@gmail.com>
2025-11-21 17:16:29 +0700

    setup swagger api docs (#45)
    Co-authored-by: phuongld <phuongld@rabiloo.com>

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,18 +1,20 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 
-const userSchema = new mongoose.Schema({
-  fullName: String,
-  email: String,
-  password: { type: String, required: true, select: false },
-  token: String,
-  phone: String,
-  address: String,
-  avatar: String,
-  status: {
-    type: String,
-    enum: ["active", "inactive", "suspended"],
-    default: "active",
+const userSchema = new mongoose.Schema(
+  {
+    fullName: String,
+    email: String,
+    password: { type: String, required: true, select: false },
+    token: String,
+    phone: String,
+    address: String,
+    avatar: String,
+    status: {
+      type: String,
+      enum: ['active', 'inactive', 'suspended'],
+      default: 'active',
+    },
   },
   {
     timestamps: true,
